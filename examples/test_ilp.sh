28be184636414d9f21923c5172abb1a9e6f78993
Joseph Gardi <jgardi@hmc.edu>
2018-05-11 10:37:40 -0700

    removed check_correct_reconciliation

--- examples/test_ilp.sh
+++ examples/test_ilp.sh
@@ -1,5 +1,5 @@
 # run dlclp with a coaldup cost of 10
 dlclp -s config/paper.stree \
-	    -S config/paper.smap \
+	    -S config/paper.smap -I .coal.tree \
 	        data/paper/0/0.coal.tree -K 10
 
